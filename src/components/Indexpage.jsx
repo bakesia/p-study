9f183e52aa0e9f3807654cf19e0996a65191553b
bakesia <qpun0912@gmail.com>
2025-08-21 21:51:54 +0900

    08.21 인덱스 페이지 디자인

--- src/components/Indexpage.jsx
+++ src/components/Indexpage.jsx
@@ -10,24 +10,36 @@ export default function Indexpage() {
   };
 
   const handleq = () => {
-    navigate("/question", { state:{nickname} });
+    if (nickname.trim() === "") {
+      alert("닉네임을 입력해주세요.");
+      return;
+    }
+    navigate("/question", { state: { nickname } });
   };
 
   return (
-    <div className="flex flex-col justify-center items-center my-10">
-      <h1 className="font-bold text-2xl">에겐남, 테토녀 테스트</h1>
-      <h1 className="text-base">
-        당신은 에겐일까요, 테토일까요? 테스트를 통해 알아보아요.
+    <div className="flex flex-col justify-center items-center bg-slate-100 py-10">
+      <h1 className="font-bold text-3xl mb-3">
+        나는 <span className="text-pink-700">에겐</span>일까,{" "}
+        <span className="text-blue-800">테토</span>
+        일까?
       </h1>
-      <img src="/gender.png" alt="남녀 사진" className="w-[50%] my-8" />
+      <h1 className="text-base text-gray-600 py-1 italic border-gray-400 border-b-2 border-t-2">
+        “에겐과 테토, 두 가지 기준으로 보는 나의 성향. 지금 확인해보세요.”
+      </h1>
+      <img
+        src="/gender.png"
+        alt="남녀 사진"
+        className="w-[50%] my-8 bg-white rounded-lg p-5 shadow-xl"
+      />
       <input
         type="text"
         placeholder="닉네임을 입력하세요."
         onChange={handleNick}
-        className="flex border border-gray-500 rounded-xl p-2 justify-center items-center"
+        className="flex border border-gray-300 p-2 text-center rounded-2xl"
       />
       <button
-        className="my-5 bg-blue-500 rounded-xl p-2 text-white hover:bg-blue-600"
+        className="my-5 bg-blue-500 rounded-xl p-2 px-3 text-white font-bold hover:shadow-lg hover:bg-blue-600"
         onClick={handleq}
       >
         테스트 하러 가기
--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -1,14 +1,14 @@
-import { useLocation,useNavigate} from "react-router-dom"; 
+import { useLocation, useNavigate } from "react-router-dom";
 import ImgBox from "./ImgBox";
 import ResultSub from "./ResultSub";
-import ButtonList from "./Button_list"
+import ButtonList from "./Button_list";
 
-export default function ResultPage() { 
-  const { state } = useLocation(); 
+export default function ResultPage() {
+  const { state } = useLocation();
   const { nickname, finalType, gender, finalResult } = state || {};
   const navigate = useNavigate();
 
-   const resultData = {
+  const resultData = {
     에겐남: {
       img: "",
       title: "에겐남",
@@ -32,29 +32,29 @@ export default function ResultPage() {
   };
 
   const current = finalResult ? resultData[finalResult] : null;
-  const GoHome = () =>{
-    navigate("/")
-  }
+  const GoHome = () => {
+    navigate("/");
+  };
   return (
     <div>
-      
-    <main className="result-main">
-      
-      <section className="result-card">
-        <aside className="result-media">
-           <ImgBox src={current.img} alt={current.title} />
-        </aside>
-        <h1 className="result-h1">
-        {nickname ? nickname + " 님의 결과는 " + finalResult + " 입니다 " : " 결과 페이지 "}
-      </h1>
+      <main className="result-main">
+        <section className="result-card">
+          <aside className="result-media">
+            <ImgBox src={current.img} alt={current.title} />
+          </aside>
+          <h1 className="result-h1">
+            {nickname
+              ? nickname + " 님의 결과는 " + finalResult + " 입니다 "
+              : " 결과 페이지 "}
+          </h1>
 
-        <section className="result-content">
-           <ResultSub variant="title" text={current.title} />
-          <ResultSub variant="desc" text={current.desc} />
+          <section className="result-content">
+            <ResultSub variant="title" text={current.title} />
+            <ResultSub variant="desc" text={current.desc} />
+          </section>
+          <ButtonList className="result_Button" onClick={GoHome} />
         </section>
-      <ButtonList className="result_Button" onClick={GoHome}/>
-      </section> 
-    </main>
+      </main>
     </div>
   );
 }
